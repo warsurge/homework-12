e11d98f4f2b56819237868fb58ea8d2c84ec28c6
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-04-20 20:40:39 -0500

    added what functions to use for script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # homework-12
 Employee Tracker
+
+
+As a business owner
+I want to be able to view and manage the departments, roles, and employees in my company
+So that I can organize and plan my business
--- script.js
+++ script.js
@@ -4,17 +4,25 @@ const inquirer = require("inquirer");
 // -- Build a command-line application that at a minimum allows the user to:
 
 // -- Add departments, roles, employees
+function addEmployee()
+
 
 // -- View departments, roles, employees
-
+function viewAll()
+viewDepartment()
+viewRole()
 // -- Update employee roles
+function updateEmployee()
+
+
 
 // -- Bonus points if you're able to:
 
 // -- Update employee managers
-
+// updateManager()
 // -- View employees by manager
-
+// viewByManager()
 // -- Delete departments, roles, and employees
-
+// function deleteEmployee()
 // -- View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+// totalSalaries()
